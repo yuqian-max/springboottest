5f2e0ec930095679a14ce0022167d3082696ddc2
zoe9804 <x900124@163.com>
2020-02-23 19:35:21 +0800

    user entity repository

--- src/main/java/com/springboottest/springboottest/entity/User.java
+++ src/main/java/com/springboottest/springboottest/entity/User.java
@@ -1,4 +1,19 @@
 package com.springboottest.springboottest.entity;
 
+import lombok.Data;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    private Integer user_id;
+    private String user_password;
+    private String user_name;
+
+
 }
