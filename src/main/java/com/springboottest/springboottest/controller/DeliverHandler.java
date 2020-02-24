9623e85477713127856ac92211dacf187ce03908
zoe9804 <x900124@163.com>
2020-02-24 18:43:36 +0800

    new project tset

--- src/main/java/com/springboottest/springboottest/controller/DeliverHandler.java
+++ src/main/java/com/springboottest/springboottest/controller/DeliverHandler.java
@@ -3,11 +3,13 @@ package com.springboottest.springboottest.controller;
 import com.springboottest.springboottest.entity.Deliver;
 import com.springboottest.springboottest.repository.DeliverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/deliver")
@@ -15,8 +17,9 @@ public class DeliverHandler {
     @Autowired
     private DeliverRepository deliverRepository;
 
-    @GetMapping("/findAll")
-    public List<Deliver>findAll(){
-        return deliverRepository.findAll();
+    @GetMapping("/findAll/{page}/{size}")
+    public Page<Deliver> findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size){
+        PageRequest request = PageRequest.of(page,size);
+        return deliverRepository.findAll(request);
     }
 }
--- src/test/java/com/springboottest/springboottest/SpringboottestApplicationTests.java
+++ src/test/java/com/springboottest/springboottest/SpringboottestApplicationTests.java
@@ -1,13 +1,24 @@
 package com.springboottest.springboottest;
 
+import com.springboottest.springboottest.entity.Deliver;
+import com.springboottest.springboottest.repository.DeliverRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 
 @SpringBootTest
 class SpringboottestApplicationTests {
+    @Autowired
+    private DeliverRepository repository;
 
     @Test
     void contextLoads() {
+        PageRequest pageRequest = PageRequest.of(0,3);
+        Page<Deliver> page = repository.findAll(pageRequest);
+        int i = 0;
     }
 
 }
